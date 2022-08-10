e83ace0d67c2e3b991961ce8b18976046fb49e61
Zzu-h <juho7785@naver.com>
2022-08-10 12:52:45 +0900

    feat: remote dto 생성
    - Remote dto 생성

---
+++ app/src/main/java/com/woowa/banchan/data/remote/dto/DetailItem.kt
@@ -0,0 +1,23 @@
+package com.woowa.banchan.data.remote.dto
+
+
+import com.google.gson.annotations.SerializedName
+
+data class DetailItem(
+    @SerializedName("delivery_fee")
+    val deliveryFee: String,
+    @SerializedName("delivery_info")
+    val deliveryInfo: String,
+    @SerializedName("detail_section")
+    val detailSection: List<String>,
+    @SerializedName("point")
+    val point: String,
+    @SerializedName("prices")
+    val prices: List<String>,
+    @SerializedName("product_description")
+    val productDescription: String,
+    @SerializedName("thumb_images")
+    val thumbImages: List<String>,
+    @SerializedName("top_image")
+    val topImage: String
+)
