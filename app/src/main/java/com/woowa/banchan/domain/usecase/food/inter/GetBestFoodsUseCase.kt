d234294bb56d5659120bbd0b5bb31a6131ded424
Zzu-h <juho7785@naver.com>
2022-08-11 11:29:36 +0900

    refactor: uiState 제네릭 변환
    - uiState를 제네릭 타입으로 변환한다.
    - 이는 다음의 이유를 가짐
       - view에서 데이터를 타입캐스팅 과정에서 실패할 수 있다.
       - 이는 ui layer에서 하는 작업이 아니라고 판단
       - 따라서, 제네릭 타입으로 변환해 flow 상에서 타입을 그대로 전달 받기를 수행함

--- app/src/main/java/com/woowa/banchan/domain/usecase/food/inter/GetBestFoodsUseCase.kt
+++ app/src/main/java/com/woowa/banchan/domain/usecase/food/inter/GetBestFoodsUseCase.kt
@@ -1,9 +1,10 @@
 package com.woowa.banchan.domain.usecase.food.inter
 
+import com.woowa.banchan.data.remote.dto.BestFood
 import com.woowa.banchan.ui.common.uistate.UiState
 import kotlinx.coroutines.flow.Flow
 
 interface GetBestFoodsUseCase {
 
-    suspend operator fun invoke(): Flow<UiState>
+    suspend operator fun invoke(): Flow<UiState<BestFood>>
 }
--- app/src/main/java/com/woowa/banchan/domain/usecase/food/inter/GetFoodsUseCase.kt
+++ app/src/main/java/com/woowa/banchan/domain/usecase/food/inter/GetFoodsUseCase.kt
@@ -1,9 +1,10 @@
 package com.woowa.banchan.domain.usecase.food.inter
 
+import com.woowa.banchan.data.remote.dto.Food
 import com.woowa.banchan.ui.common.uistate.UiState
 import kotlinx.coroutines.flow.Flow
 
 interface GetFoodsUseCase {
 
-    suspend operator fun invoke(type: String): Flow<UiState>
+    suspend operator fun invoke(type: String): Flow<UiState<Food>>
 }
