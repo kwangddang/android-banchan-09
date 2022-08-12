bbf6ee5f78c44bf49f2fb8ea7b70ab106f73a272
kwangddang <kwangddang12@naver.com>
2022-08-12 23:29:41 +0900

    ui: Spinner 디자인
    - Spinner 간격 수정, 클릭시 글자 색 굵게

--- app/src/main/java/com/woowa/banchan/ui/home/main/adapter/viewholder/MainHeaderViewHolder.kt
+++ app/src/main/java/com/woowa/banchan/ui/home/main/adapter/viewholder/MainHeaderViewHolder.kt
@@ -1,5 +1,7 @@
 package com.woowa.banchan.ui.home.main.adapter.viewholder
 
+import android.view.View
+import android.widget.AdapterView
 import android.widget.RadioGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.woowa.banchan.databinding.ItemMainHeaderBinding
@@ -8,7 +10,15 @@ import com.woowa.banchan.ui.home.main.adapter.SpinnerAdapter
 class MainHeaderViewHolder(private val binding: ItemMainHeaderBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun bind(checkedChangeListener: (RadioGroup, Int) -> Unit) {
-        binding.spinnerSort.adapter = SpinnerAdapter(binding.spinnerSort.context)
+        val spinnerAdapter = SpinnerAdapter(binding.spinnerSort.context)
+        binding.spinnerSort.adapter = spinnerAdapter
+        binding.spinnerSort.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                spinnerAdapter.selectedPosition = position
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        }
         binding.rgManager.setOnCheckedChangeListener { group, checkedId ->
             checkedChangeListener(group, checkedId)
         }
