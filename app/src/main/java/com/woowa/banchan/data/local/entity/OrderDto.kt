e664921ff0255cb3ecfcb104d94b4a96218a6813
Zzu-h <juho7785@naver.com>
2022-08-12 15:32:05 +0900

    refactor: Dto 및 Database Table 관리
    - Data를 받아오고 보내주는 data class의 명을 dto로 변경함
    - database의 명을 명시해줌
    - 사용하지 않는 instance 변수를 제거

--- app/src/main/java/com/woowa/banchan/data/local/entity/OrderDto.kt
+++ app/src/main/java/com/woowa/banchan/data/local/entity/OrderDto.kt
@@ -3,11 +3,13 @@ package com.woowa.banchan.data.local.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.woowa.banchan.data.local.BanchanDataBase.Companion.orderTable
+import java.util.*
 
-@Entity
-data class OrderItem(
+@Entity(tableName = orderTable)
+data class OrderDto(
     @PrimaryKey(autoGenerate = true) val id: Long,
-    @ColumnInfo(name = "order_id") val orderId: Long,
+    @ColumnInfo(name = "time") val time: Date,
     @ColumnInfo(name = "count") val count: Int,
     @ColumnInfo(name = "price") val price: Int,
     @ColumnInfo(name = "title") val title: String,
--- app/src/main/java/com/woowa/banchan/data/local/entity/OrderItemDto.kt
+++ app/src/main/java/com/woowa/banchan/data/local/entity/OrderItemDto.kt
@@ -3,12 +3,12 @@ package com.woowa.banchan.data.local.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.util.*
+import com.woowa.banchan.data.local.BanchanDataBase.Companion.orderItemTable
 
-@Entity
-data class Order(
+@Entity(tableName = orderItemTable)
+data class OrderItemDto(
     @PrimaryKey(autoGenerate = true) val id: Long,
-    @ColumnInfo(name = "time") val time: Date,
+    @ColumnInfo(name = "order_id") val orderId: Long,
     @ColumnInfo(name = "count") val count: Int,
     @ColumnInfo(name = "price") val price: Int,
     @ColumnInfo(name = "title") val title: String,
