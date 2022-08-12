76d79e11e21ea336951ebb6e64f339562edaa032
Zzu-h <juho7785@naver.com>
2022-08-12 15:27:18 +0900

    refactor: Coroutine Scope에서 viewModel Scope으로 변경
    - ViewModel Scope으로 변경하기 위해 database에 접근하기 직전에 WorkerThread로 변경한다.

--- app/src/main/java/com/woowa/banchan/data/local/datasource/cart/CartDataSourceImpl.kt
+++ app/src/main/java/com/woowa/banchan/data/local/datasource/cart/CartDataSourceImpl.kt
@@ -1,5 +1,6 @@
 package com.woowa.banchan.data.local.datasource.cart
 
+import androidx.annotation.WorkerThread
 import com.woowa.banchan.data.local.dao.CartDao
 import com.woowa.banchan.data.local.entity.CartDto
 import javax.inject.Inject
@@ -8,6 +9,7 @@ class CartDataSourceImpl @Inject constructor(
     private val cartDao: CartDao
 ) : CartDataSource {
 
+    @WorkerThread
     override suspend fun getCartList(): Result<List<CartDto>> =
         runCatching { cartDao.getCartList() }
 }
--- app/src/main/java/com/woowa/banchan/data/local/datasource/recent/RecentDataSourceImpl.kt
+++ app/src/main/java/com/woowa/banchan/data/local/datasource/recent/RecentDataSourceImpl.kt
@@ -1,5 +1,6 @@
 package com.woowa.banchan.data.local.datasource.recent
 
+import androidx.annotation.WorkerThread
 import com.woowa.banchan.data.local.dao.RecentDao
 import com.woowa.banchan.data.local.entity.RecentDto
 import javax.inject.Inject
@@ -8,6 +9,7 @@ class RecentDataSourceImpl @Inject constructor(
     private val recentDao: RecentDao
 ) : RecentDataSource {
 
+    @WorkerThread
     override suspend fun getRecentList(): Result<List<RecentDto>> =
         runCatching { recentDao.getRecentList() }
 }
