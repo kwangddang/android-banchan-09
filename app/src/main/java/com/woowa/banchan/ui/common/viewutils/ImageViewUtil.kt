483ca1546ff018b10c3ae386722e85362a78fb60
kwangddang <kwangddang12@naver.com>
2022-08-18 19:50:11 +0900

    feat: FoodItem이 장바구니에 있다면 아이콘 변경
    - Cart에 있는 Food는 체크표시로 아이콘을 변경하였습니다.

--- app/src/main/java/com/woowa/banchan/ui/common/viewutils/ImageViewUtil.kt
+++ app/src/main/java/com/woowa/banchan/ui/common/viewutils/ImageViewUtil.kt
@@ -3,6 +3,7 @@ package com.woowa.banchan.ui.common.viewutils
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.woowa.banchan.R
 
 @BindingAdapter("image")
 fun ImageView.setImage(url: String?) {
@@ -10,5 +11,16 @@ fun ImageView.setImage(url: String?) {
         Glide.with(context)
             .load(url)
             .into(this)
+    }
+}
+
+@BindingAdapter("cartIcon")
+fun ImageView.setCartIcon(isCart: Boolean) {
+    if(isCart) {
+        setBackgroundResource(R.drawable.background_circle_accent)
+        setImageResource(R.drawable.ic_check_white)
+    } else {
+        setBackgroundResource(R.drawable.background_circle_white)
+        setImageResource(R.drawable.ic_cart)
     }
 }
