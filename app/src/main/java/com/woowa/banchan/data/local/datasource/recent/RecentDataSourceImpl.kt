d7f24dc7b3b79029f01586709d4e55e056124f9a
Zzu-h <juho7785@naver.com>
2022-08-20 21:25:11 +0900

    feat: 최근 본 상품 클릭 시 Detail 화면 이동
    -  최근 본 상품 테이블 갱신 시점 구현
      - 데이터들을 불러올 때 24시간 전인 데이터들은 그 순간 delete 요청을 수행해 갱신한다.

--- app/src/main/java/com/woowa/banchan/data/local/datasource/recent/RecentDataSourceImpl.kt
+++ app/src/main/java/com/woowa/banchan/data/local/datasource/recent/RecentDataSourceImpl.kt
@@ -2,6 +2,7 @@ package com.woowa.banchan.data.local.datasource.recent
 
 import com.woowa.banchan.data.local.dao.RecentDao
 import com.woowa.banchan.data.local.entity.RecentDto
+import java.util.*
 import javax.inject.Inject
 
 class RecentDataSourceImpl @Inject constructor(
@@ -9,7 +10,20 @@ class RecentDataSourceImpl @Inject constructor(
 ) : RecentDataSource {
 
     override suspend fun getRecentList(): Result<List<RecentDto>> =
-        runCatching { recentDao.getRecentList() }
+        runCatching {
+            val list = recentDao.getRecentList().toMutableList()
+            val curCalendar = Calendar.getInstance()
+                .apply { time = Date() }
+            val calendar = Calendar.getInstance()
+            val retList = mutableListOf<RecentDto>()
+            list.forEach {
+                calendar.time = it.time
+                if (calendar.get(Calendar.DAY_OF_WEEK) != curCalendar.get(Calendar.DAY_OF_WEEK))
+                    recentDao.delete(it)
+                else retList.add(it)
+            }
+            retList
+        }
 
     override suspend fun insertRecent(recentDto: RecentDto): Result<Unit> =
         runCatching { recentDao.insert(recentDto) }
