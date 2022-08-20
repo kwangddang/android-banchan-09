28f7e14d9c390591ce396daca43e9d265f2f01cb
Zzu-h <juho7785@naver.com>
2022-08-20 19:39:28 +0900

    feat: 전체 선택 기능 구현
    - 전체 선택 기능 구현
    - 전체 리스트에 대한 flag로 현재 선택된 아이템들에 대한 내용을 기록한다.
    - index -> 1 일경우 1번째자리에 bit를 토글해서 기록
    - 이를 viewholder에서 확인 후 변경한다.

--- app/src/main/java/com/woowa/banchan/ui/cart/cart/adapter/viewholder/CheckHeaderViewHolder.kt
+++ app/src/main/java/com/woowa/banchan/ui/cart/cart/adapter/viewholder/CheckHeaderViewHolder.kt
@@ -5,14 +5,16 @@ import com.woowa.banchan.databinding.ItemCartCheckHeaderBinding
 
 class CheckHeaderViewHolder(
     private val binding: ItemCartCheckHeaderBinding,
-    private val onClickRemoveSelection: () -> Unit = {},
-    private val onClickReleaseSelection: () -> Unit = {}
+    private val onClickRemoveSelection: () -> Unit,
+    private val onClickAllSelection: () -> Unit,
+    private val onClickReleaseSelection: () -> Unit
 ) :
     RecyclerView.ViewHolder(binding.root) {
 
-
-    fun bind() {
+    fun bind(checkStateFlag: Int) {
+        binding.checkStateFlag = checkStateFlag != 0
         binding.tvRemoveSelection.setOnClickListener { onClickRemoveSelection() }
+        binding.tvAllChecked.setOnClickListener { onClickAllSelection() }
         binding.tvReleaseChecked.setOnClickListener { onClickReleaseSelection() }
     }
 }
