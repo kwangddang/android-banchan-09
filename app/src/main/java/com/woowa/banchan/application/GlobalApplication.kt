d19247fd37435f623208c727ca5fd6a128dd9e12
kwangddang <kwangddang12@naver.com>
2022-08-21 16:53:09 +0900

    chore: work manager gradle 추가

--- app/src/main/java/com/woowa/banchan/application/GlobalApplication.kt
+++ app/src/main/java/com/woowa/banchan/application/GlobalApplication.kt
@@ -1,7 +1,19 @@
 package com.woowa.banchan.application
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class GlobalApplication : Application()
+class GlobalApplication : Application(), Configuration.Provider {
+
+    @Inject lateinit var workerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration(): Configuration =
+        Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+
+}
---
+++ app/src/main/java/com/woowa/banchan/ui/worker/CoroutineOrderWorker.kt
@@ -0,0 +1,21 @@
+package com.woowa.banchan.ui.worker
+
+import android.content.Context
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.woowa.banchan.domain.usecase.cart.inter.InsertCartUseCase
+import com.woowa.banchan.domain.usecase.order.inter.GetTotalOrderUseCase
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+
+class CoroutineOrderWorker @AssistedInject constructor(
+    @Assisted context: Context,
+    @Assisted params: WorkerParameters,
+    private val updateOrderUseCase: GetTotalOrderUseCase
+) : CoroutineWorker(context,params) {
+
+    override suspend fun doWork(): Result {
+        TODO("Not yet implemented")
+    }
+}
