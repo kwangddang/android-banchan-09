6b5abe1dc8c849d6af3e5387a2d6a14ca74631ff
Zzu-h <juho7785@naver.com>
2022-08-22 16:06:44 +0900

    refactor: DataSource와 Repository의 역할 재정의
    - DataSource는 데이터를 가져오고 전달하는 역할의 객체로 둔다.
    - Repository에서 필요로되는 데이터를 파싱하고 적절한 데이터를 전달한다.
    - 이 역할을 다시 정의하며 코드를 수정한다.

--- app/src/main/java/com/woowa/banchan/data/local/datasource/recent/RecentDataSourceImpl.kt
+++ app/src/main/java/com/woowa/banchan/data/local/datasource/recent/RecentDataSourceImpl.kt
@@ -2,7 +2,6 @@ package com.woowa.banchan.data.local.datasource.recent
 
 import com.woowa.banchan.data.local.dao.RecentDao
 import com.woowa.banchan.data.local.entity.RecentDto
-import java.util.*
 import javax.inject.Inject
 
 class RecentDataSourceImpl @Inject constructor(
@@ -10,20 +9,10 @@ class RecentDataSourceImpl @Inject constructor(
 ) : RecentDataSource {
 
     override suspend fun getRecentList(): Result<List<RecentDto>> =
-        runCatching {
-            val list = recentDao.getRecentList().toMutableList()
-            val curCalendar = Calendar.getInstance()
-                .apply { time = Date() }
-            val calendar = Calendar.getInstance()
-            val retList = mutableListOf<RecentDto>()
-            list.forEach {
-                calendar.time = it.time
-                if (calendar.get(Calendar.DAY_OF_WEEK) != curCalendar.get(Calendar.DAY_OF_WEEK))
-                    recentDao.delete(it)
-                else retList.add(it)
-            }
-            retList
-        }
+        runCatching { recentDao.getRecentList() }
+
+    override suspend fun deleteRecent(recentDto: RecentDto): Result<Unit> =
+        runCatching { recentDao.delete(recentDto) }
 
     override suspend fun insertRecent(recentDto: RecentDto): Result<Unit> =
         runCatching { recentDao.insert(recentDto) }
--- app/src/main/java/com/woowa/banchan/data/local/repository/RecentRepositoryImpl.kt
+++ app/src/main/java/com/woowa/banchan/data/local/repository/RecentRepositoryImpl.kt
@@ -1,20 +1,33 @@
 package com.woowa.banchan.data.local.repository
 
 import com.woowa.banchan.data.local.datasource.recent.RecentDataSource
+import com.woowa.banchan.data.local.entity.RecentDto
 import com.woowa.banchan.data.local.entity.toRecent
 import com.woowa.banchan.data.local.entity.toRecentDto
 import com.woowa.banchan.domain.model.Recent
 import com.woowa.banchan.domain.repository.RecentRepository
+import java.util.*
 import javax.inject.Inject
 
 class RecentRepositoryImpl @Inject constructor(
     private val recentDataSource: RecentDataSource
 ) : RecentRepository {
 
-    override suspend fun getRecentList(): Result<List<Recent>> {
-        val list = recentDataSource.getRecentList().getOrThrow()
-        return runCatching { list.map { it.toRecent() } }
-    }
+    override suspend fun getRecentList(): Result<List<Recent>> =
+        runCatching {
+            val list = recentDataSource.getRecentList().getOrThrow()
+            val curCalendar = Calendar.getInstance()
+                .apply { time = Date() }
+            val calendar = Calendar.getInstance()
+            val retList = mutableListOf<RecentDto>()
+            list.forEach {
+                calendar.time = it.time
+                if (calendar.get(Calendar.DAY_OF_WEEK) != curCalendar.get(Calendar.DAY_OF_WEEK))
+                    recentDataSource.deleteRecent(it)
+                else retList.add(it)
+            }
+            retList.map { it.toRecent() }
+        }
 
     override suspend fun insertRecent(recent: Recent): Result<Unit> =
         runCatching { recentDataSource.insertRecent(recent.toRecentDto()) }
