59e56755087992792e08b4211337c2d95332cf95
kwangddang <kwangddang12@naver.com>
2022-08-24 01:40:47 +0900

    refactor: CartCompleteFragment 클릭 이벤트 이전
    - confirm, continue 이벤트

---
+++ app/src/main/java/com/woowa/banchan/ui/common/popup/CartCompleteViewModel.kt
@@ -0,0 +1,24 @@
+package com.woowa.banchan.ui.common.popup
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.woowa.banchan.ui.common.event.SingleEvent
+import com.woowa.banchan.ui.common.event.setEvent
+
+class CartCompleteViewModel : ViewModel(){
+
+    private val _confirmClickEvent = MutableLiveData<SingleEvent<Unit>>()
+    val confirmClickEvent: LiveData<SingleEvent<Unit>> get() = _confirmClickEvent
+
+    private val _continueClickEvent = MutableLiveData<SingleEvent<Unit>>()
+    val continueClickEvent: LiveData<SingleEvent<Unit>> get() = _continueClickEvent
+
+    fun setConfirmClickEvent() {
+        _confirmClickEvent.setEvent()
+    }
+
+    fun setContinueClickEvent() {
+        _continueClickEvent.setEvent()
+    }
+}
