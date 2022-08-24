efac1dd5e82a411f3251d970e511a3862c4ae3ac
kwangddang <kwangddang12@naver.com>
2022-08-24 18:00:22 +0900

    feat: 알림 발생시 배송 상태 업데이트
    - AlarmManager의 onReceive()가 끝날 때,
    - WorkManager를 호출하여 배송 상태 업데이트

--- app/src/main/java/com/woowa/banchan/service/worker/OrderReceiver.kt
+++ app/src/main/java/com/woowa/banchan/service/worker/OrderReceiver.kt
@@ -11,9 +11,12 @@ import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.OutOfQuotaPolicy
+import androidx.work.WorkManager
 import com.woowa.banchan.R
 import com.woowa.banchan.ui.order.OrderActivity
-import com.woowa.banchan.ui.order.detail.OrderDetailActivity
 
 class OrderReceiver : BroadcastReceiver(){
     override fun onReceive(context: Context?, intent: Intent?) {
@@ -24,27 +27,38 @@ class OrderReceiver : BroadcastReceiver(){
         val orderName = context.getString(R.string.order_name)
         val orderContent = context.getString(R.string.order_content)
 
-        createNotificationChannel(context, orderName, orderChannelId, orderTitle, orderContent)
-
         val newIntent = Intent(context, OrderActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         }
         val pendingIntent = PendingIntent.getActivity(context, 0,newIntent, FLAG_UPDATE_CURRENT or FLAG_IMMUTABLE)
 
-        var builder = NotificationCompat.Builder(context, orderChannelId)
-            .setSmallIcon(R.mipmap.ic_app_logo_accent_round)
-            .setContentTitle(orderTitle)
-            .setContentText(orderContent)
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .setContentIntent(pendingIntent)
-            .setAutoCancel(true)
+        val builder = createNotificationBuilder(context, orderChannelId, orderTitle, orderContent, pendingIntent)
+
+        createNotificationChannel(context, orderName, orderChannelId, orderContent)
 
         with(NotificationManagerCompat.from(context)) {
             notify(orderChannelId.toInt(), builder.build())
         }
+
+        startOrderWork(context,orderId)
     }
 
-    private fun createNotificationChannel(context: Context, orderName: String, orderChannelId: String, orderTitle: String, orderContent: String) {
+    private fun createNotificationBuilder(
+        context: Context?,
+        orderChannelId: String,
+        orderTitle: String,
+        orderContent: String,
+        pendingIntent: PendingIntent?
+    ) =
+        NotificationCompat.Builder(context!!, orderChannelId)
+        .setSmallIcon(R.mipmap.ic_app_logo_accent_round)
+        .setContentTitle(orderTitle)
+        .setContentText(orderContent)
+        .setPriority(NotificationCompat.PRIORITY_HIGH)
+        .setContentIntent(pendingIntent)
+        .setAutoCancel(true)
+
+    private fun createNotificationChannel(context: Context, orderName: String, orderChannelId: String, orderContent: String) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(orderChannelId, orderName, NotificationManager.IMPORTANCE_HIGH).apply {
                 description = orderContent
@@ -54,5 +68,16 @@ class OrderReceiver : BroadcastReceiver(){
                 context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
         }
+    }
+
+    private fun startOrderWork(context: Context?, orderId: Long) {
+        val workManager = WorkManager.getInstance(context!!)
+
+        workManager.enqueue(
+            OneTimeWorkRequestBuilder<OrderWorker>()
+                .setInputData(Data.Builder().putLong("id", orderId).build())
+                .setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
+                .build()
+        )
     }
 }
