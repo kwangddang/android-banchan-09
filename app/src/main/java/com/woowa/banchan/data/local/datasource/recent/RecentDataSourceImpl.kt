12776655c166e85c6d317df96d74dd03b73fb487
kwangddang <kwangddang12@naver.com>
2022-08-29 13:48:36 +0900

    fix: Recent가 MainThread에서 지워지던 버그 수정

--- app/src/main/java/com/woowa/banchan/data/local/datasource/recent/RecentDataSourceImpl.kt
+++ app/src/main/java/com/woowa/banchan/data/local/datasource/recent/RecentDataSourceImpl.kt
@@ -2,7 +2,9 @@ package com.woowa.banchan.data.local.datasource.recent
 
 import com.woowa.banchan.data.local.dao.RecentDao
 import com.woowa.banchan.data.local.entity.RecentDto
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class RecentDataSourceImpl @Inject constructor(
@@ -12,7 +14,7 @@ class RecentDataSourceImpl @Inject constructor(
     override suspend fun getRecentList(): Flow<List<RecentDto>> =
         recentDao.getRecentList()
 
-    override suspend fun deleteRecent(recentDto: RecentDto) {
+    override suspend fun deleteRecent(recentDto: RecentDto) = withContext(Dispatchers.IO) {
         recentDao.delete(recentDto)
     }
 
